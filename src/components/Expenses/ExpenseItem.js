10dba309e74afa592840723b939c1a2ad4ef9a11
soncom <tngud124@gmail.com>
2022-07-12 17:54:10 +0900

    STUDY : react event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,7 +7,6 @@ import Card from '../UI/Card';
 const ExpenseItem = (props) => {
 
     const [title, setTitle] = useState(props.title);
-    console.log('')
     
     const clickHandler = () => {
         setTitle('Update!');
@@ -19,7 +18,7 @@ const ExpenseItem = (props) => {
         <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
